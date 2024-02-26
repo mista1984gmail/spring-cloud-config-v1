77dbaf12ca5e62ea18dd43ede8954896f7a94be2
mista1984gmail <mista1984@gmail.com>
2024-02-26 11:46:36 +0300

    Initial commit

---
+++ src/test/java/com/example/springcloudconfigv1/SpringCloudConfigV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springcloudconfigv1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudConfigV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
